dfdd459f95af6cdc05218eacc33e07d438a050c1
Nora Drentje <webbestelling@protonmail.com>
2025-12-01 16:42:32 +0100

    added schema

---
+++ app/models/project_yarn.rb
@@ -0,0 +1,4 @@
+class ProjectYarn < ApplicationRecord
+  belongs_to :yarn_id
+  belongs_to :project_id
+end
---
+++ test/models/project_yarn_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ProjectYarnTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
