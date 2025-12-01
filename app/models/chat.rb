f3b138769bbfbac1357178d0cc98f53488187be9
antonio_sartor <Antoniosartor275@gmail.com>
2025-12-01 17:48:43 +0100

    seed complete

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,3 @@
 class Chat < ApplicationRecord
-  belongs_to :user_id
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_01_153904) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_01_164126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,9 +73,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_01_153904) do
   end
 
   create_table "yarns", force: :cascade do |t|
-    t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "material"
   end
 
   add_foreign_key "chats", "users"
