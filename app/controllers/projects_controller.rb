9dd373d4d4f107c8913f5b49dfd56300ead2d48d
Nora Drentje <webbestelling@protonmail.com>
2025-12-02 12:30:52 +0100

    added stying to form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,19 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    raise
+    @project = Project.new(project_params)
+    @project.user = current_user
+
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :designer, :category, :needle_size, :product_size, :difficulty)
   end
 end
