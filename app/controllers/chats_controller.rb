b4ac7bef3827e40391728c2d19fbb0c22545ca8f
Louise Toussaint <Louisetoussaint4@gmail.com>
2025-12-02 16:00:47 +0100

    llm set up

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,38 @@
+# If I am a logged in user:When I go to /chats/new -> I should see Yarni welcome page to start a new chat
+# I click on the “Tallk to Yarni” form
+# When my chat is successfully created  -> Then I should be redirected to /chats/:id -> And I should see my conversation screen
+
+
+class ChatsController < ApplicationController
+  # before_action :authenticate_user!
+  # only logged in users can access new, create, show
+
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = Chat.new
+    @chat.user = current_user
+
+    @chat.save
+      # create the first assistant message
+      @chat.messages.create!(
+        role: "assistant",
+        content: "Hello, this is Yarni! I'm ready to help. What can I do for your project today?"
+      )
+  end
+
+  def show
+    @chat = Chat.find(params[:id])
+    @messages = @chat.messages.order(:created_at)
+    @message = Message.new
+    # @bottom_bar = true //to update once we have the bottom bar
+  end
+
+  private
+
+  # def chat_params
+  #   params.require(:chat).permit()
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :chats, only: :show do
+    resources :messages, only: [:new, :create]
+  end
 end
