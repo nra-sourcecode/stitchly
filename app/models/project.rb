05e184e138ce2a8bcd2ce9a1e1503d68690d5a70
Nora Drentje <webbestelling@protonmail.com>
2025-12-02 10:08:16 +0100

    fixed comment

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :project_yarns
   has_many :tasks
+  has_many :yarns, through: :project_yarns
 end
--- app/models/yarn.rb
+++ app/models/yarn.rb
@@ -1,3 +1,4 @@
 class Yarn < ApplicationRecord
   has_many :project_yarns
+  has_many :projects, through: :project_yarns
 end
