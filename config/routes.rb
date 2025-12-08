062447631019a79e8b1dd579807573d54266dd1a
Nora Drentje <webbestelling@protonmail.com>
2025-12-08 10:12:04 +0100

    progress-bar

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
  resources :projects
+ resources :tasks, only: [:update]
 root "projects#home"
 
 
@@ -16,4 +17,5 @@ root "projects#home"
 resources :chats, only: :show do
   resources :messages, only: [:new, :create]
   end
+
 end
