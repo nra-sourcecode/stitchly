78a050f4c7b41d20cb612291248a2bda789bdaa5
Nora Drentje <webbestelling@protonmail.com>
2025-12-08 13:56:18 +0100

    when all checkmarks are checked, project gets updated to finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(state: !@task.state)
+    @task.project.project_complete?
     redirect_to project_path(@task.project)
   end
 end
