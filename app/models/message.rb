4a54487badc61cd9f332d4dfd6084f4844447075
Nora Drentje <webbestelling@protonmail.com>
2025-12-09 16:30:08 +0100

    yarni is no longer typing in our field

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :chat
 
-  validates :chat_id, presence: true, numericality: { only_integer: true }
-  validates :role, presence: true, inclusion: { in: ["user", "assistant"] }
-  validates :content, presence: true
+  # validates :chat_id, presence: true, numericality: { only_integer: true }
+  # validates :role, presence: true, inclusion: { in: ["user", "assistant"] }
+  # validates :content, presence: true
 
   scope :by_user, -> { where(role: "user") }
   scope :by_assistant, -> { where(role: "assistant") }
