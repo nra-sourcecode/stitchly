803bc59fb719f76521b7dd2b1cdac5ab22eb3836
Nora Drentje <webbestelling@protonmail.com>
2025-12-10 15:12:48 +0100

    fixed javascript error

--- app/javascript/controllers/enter_controller.js
+++ app/javascript/controllers/enter_controller.js
@@ -2,26 +2,26 @@
 
 // });
 
-// import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus";
 
-// export default class extends Controller {
-//   connect() {
-//     const messageInput = document.getElementById("messageInput");
-//     const form = document.querySelector(".simple_form");
-//     messageInput.addEventListener("keyup", function (event) {
-//       // Check if the Enter key is pressed
-//       console.log("typing");
-//       if (event.key === "Enter") {
-//         // Prevent the default action of creating a new line
-//         event.preventDefault();
-//         // Submit the form
-//         if (messageInput.value.trim() !== "") {
-//           // Make sure there's input before sending
-//           console.log(form);
-//           form.submit();
-//           messageInput.value = "";
-//         }
-//       }
-//     });
-//   }
-// }
+export default class extends Controller {
+  connect() {}
+  //     const messageInput = document.getElementById("messageInput");
+  //     const form = document.querySelector(".simple_form");
+  //     messageInput.addEventListener("keyup", function (event) {
+  //       // Check if the Enter key is pressed
+  //       console.log("typing");
+  //       if (event.key === "Enter") {
+  //         // Prevent the default action of creating a new line
+  //         event.preventDefault();
+  //         // Submit the form
+  //         if (messageInput.value.trim() !== "") {
+  //           // Make sure there's input before sending
+  //           console.log(form);
+  //           form.submit();
+  //           messageInput.value = "";
+  //         }
+  //       }
+  //     });
+  //   }
+}
