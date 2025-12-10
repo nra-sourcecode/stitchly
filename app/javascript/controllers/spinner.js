406457ef918b6d5e5e2dbd884cfe9350f40f8832
Nora Drentje <webbestelling@protonmail.com>
2025-12-10 16:02:07 +0100

    fixing the js

--- app/javascript/controllers/spinner.js
+++ app/javascript/controllers/spinner.js
@@ -2,8 +2,9 @@
 const form = document.getElementById("new_project");
 const spinner = document.getElementById("spinner");
 
-form.addEventListener("submit", () => {
-  console.log(spinner);
-  spinner.classList.remove("d-none");
-});
+if (spinner)
+  form.addEventListener("submit", () => {
+    console.log(spinner);
+    spinner.classList.remove("d-none");
+  });
 // });
