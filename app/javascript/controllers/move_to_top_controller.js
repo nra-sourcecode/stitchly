ba1fa107e852d353fafd39a341be37b09e0bb656
Nora Drentje <webbestelling@protonmail.com>
2025-12-10 20:28:03 +0100

    fixed spinner and added to move-to-top

--- app/javascript/controllers/move_to_top_controller.js
+++ app/javascript/controllers/move_to_top_controller.js
@@ -7,9 +7,10 @@ export default class extends Controller {
     this.observer = new MutationObserver((mutations) => {
       mutations.forEach((mutation) => {
         mutation.addedNodes.forEach((node) => {
+          console.log("NODE", node);
           // Detect a turbo-stream applied update
           if (node.nodeType === 1) {
-            // console.log(node.classList[1]);
+            console.log(node.classList[1]);
             this.handleNewMessage(node);
           }
         });
@@ -24,11 +25,13 @@ export default class extends Controller {
   }
 
   handleNewMessage(node) {
-    const messages = document.getElementById("messages");
-    const assistantMessages =
-      messages.getElementsByClassName("message assistant");
-    const lastMessage = assistantMessages[assistantMessages.length - 1];
-    console.log("LAST MESSAGE = ", lastMessage);
-    lastMessage.scrollIntoView({ behavior: "smooth" });
+    if (node.classList.contains?.("assistant")) {
+      const messages = document.getElementById("messages");
+      const assistantMessages =
+        messages.getElementsByClassName("message assistant");
+      const lastMessage = assistantMessages[assistantMessages.length - 1];
+      console.log("LAST MESSAGE = ", lastMessage);
+      lastMessage.scrollIntoView({ behavior: "smooth" });
+    }
   }
 }
