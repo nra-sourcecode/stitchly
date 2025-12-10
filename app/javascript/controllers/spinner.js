e4f9110c8ffb8be5dcc03f262c9029ad8a0f81f8
Nora Drentje <webbestelling@protonmail.com>
2025-12-10 15:24:50 +0100

    spinner to edit page

--- app/javascript/controllers/spinner.js
+++ app/javascript/controllers/spinner.js
@@ -3,7 +3,7 @@ const form = document.getElementById("new_project");
 const spinner = document.getElementById("spinner");
 
 form.addEventListener("submit", () => {
-  console.log("hello");
+  console.log(spinner);
   spinner.classList.remove("d-none");
 });
 // });
